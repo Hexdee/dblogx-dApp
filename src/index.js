d5509b7fdd346384a33f73296e042a8c45a70a3c
Freedteck <freedteck@gmail.com>
2024-03-24 11:39:22 +0100

    Update html, css, and js
    Add data.json file and load its content dynamically, replace static featured contents with dynamic ones.

---
+++ src/index.js
@@ -0,0 +1,57 @@
+// const data = "data.json"
+const featuredContainer = document.querySelector(".posts");
+
+const getData = async () => {
+  const contents = await fetch("./src/data.json", { mode: "cors" });
+  const result = await contents.json();
+  return result;
+};
+
+let currentIndex = 0; 
+const getFeaturedContents = (count) => {
+//   const createdPosts = []; // Array to keep track of created posts
+  getData()
+    .then((response) => {
+      const endIndex = currentIndex + count;
+      for (let i = currentIndex; i < endIndex; i++) {
+        if (i >= response.length) break; // Exit loop if we reach end of response
+        const post = document.createElement("div");
+        post.classList.add("post");
+        post.innerHTML = `<div class="post-img"><img src="${response[i].postImg}" alt="" /></div>
+            <div class="post-details">
+              <div class="title-desc">
+                <h3 class="title">
+                  ${response[i].title}
+                </h3>
+                <p class="desc">
+                  ${response[i].content}
+                </p>
+              </div>
+              <div class="author">
+                <img src="${response[i].authorImg}" alt="" />
+                <p class="name">${response[i].authorName}</p>
+                <p class="date">${response[i].date}</p>
+              </div>
+            </div>`;
+
+        // if (!createdPosts.includes(post)) {
+          // Check if post is not already created
+          featuredContainer.appendChild(post);
+        //   createdPosts.push(post); // Add post to the list of created posts
+        // } 
+      }
+      currentIndex = endIndex; // Update current index
+    })
+    .catch((e) => {
+      console.log("Error fetching data: ", e);
+    });
+};
+
+const explore = document.getElementById('explore')
+
+explore.addEventListener('click', (e) => {
+    e.preventDefault();
+    getFeaturedContents(3); // Fetch 3 more contents
+});
+
+getFeaturedContents(3);
